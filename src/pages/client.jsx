655c1e58979fc5c2648b3eae0535937bc3b584a4
anoirsa <anouarbelilawoo@outlook.com>
2021-12-13 23:09:14 +0200

    first commit

--- src/pages/client.jsx
+++ src/pages/client.jsx
@@ -1,9 +1,12 @@
 import fetch from 'unfetch';
 
 
+// No longer used as the new method is defined in the context file
+
+/** 
 export const getAllData = async() => {
     const response = await fetch("http://localhost:8080/api/registers");
     const json = await response.json()
     
     return json;
-}
+} **/
